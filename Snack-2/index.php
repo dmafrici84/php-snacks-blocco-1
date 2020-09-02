5e3de916d5aaaa2bed0cbe5f7447609d11667e83
Daniele <dmafrici84@gmail.com>
2020-09-02 17:14:56 +0200

    Lc php-snacks-blocco-1 corretto1

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -2,6 +2,7 @@
   Snack 2
   Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"
  -->
+ 
  <?php
 
     var_dump($_GET);
@@ -29,4 +30,5 @@
     } else {
       echo "Accesso Negato";
     }
+
   ?>
--- Snack-4/index.php
+++ Snack-4/index.php
@@ -1,8 +1,10 @@
 <!--
+  Snack 4
   Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta
  -->
 
 <?php
+
   $arr = [];
 
   while (count($arr) < 15) {
